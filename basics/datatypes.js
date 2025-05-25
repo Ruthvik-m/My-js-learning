2288276fb96641195a77bed81f9d580cc3bf52d8
Ruthvik-m <msruthvik725@gmail.com>
2025-05-25 11:34:37 +0000

    second commit

---
+++ basics/datatypes.js
@@ -0,0 +1,11 @@
+
+
+let myname = "RUthvik" //String
+let age = 20 //number
+let isLoggedin = true // boolean
+
+console.log(myname)
+console.log(age)
+console.log(isLoggedin)
+
+console.log(typeof null)// typeof => checks the datatype of the varaible
---
+++ basics/variables.js
@@ -0,0 +1,12 @@
+const id = 12345;
+// id = 1234567
+
+let email = "msruthvik1625@gmail.com";
+email = "ms@gmail.com";
+
+var password = "56tfg";
+password = "bhjhnm";
+
+city = "jaipur";
+city = "vizag";
+console.table([email, password, city]);
