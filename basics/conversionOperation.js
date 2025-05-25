eee86f368a61e94fb347267241debad891b0aaa4
Ruthvik-m <msruthvik725@gmail.com>
2025-05-25 12:23:19 +0000

    updated

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,13 +1,17 @@
 let score = "18hj"
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
 
 let val = Boolean(1)
-console.log(val);
+// console.log(val);
 
 
 
 // Number() -> to convert into number
 // String() -> to convert into string
 // Boolean() -> to convert into Boolean(1,"name"->true 0,""->false)
+
+
+//operations
+console.log(+true); //-> gives you 1 
